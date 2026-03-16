815c789ea953524b5b52c5a4d53c9bab513c3423
Narcil57 <narcil58elite@gmail.com>
2026-03-16 14:44:18 +0100

    merge issue

--- bot.py
+++ bot.py
@@ -26,9 +26,6 @@ class Bot(commands.Bot):
         await self.load_extension("cogs.fun")
         await self.load_extension("cogs.pause")
         await self.load_extension("cogs.calendar")
-
-        guild = discord.Object(id=GUILD_ID)
-        self.tree.clear_commands(guild=guild)
         await self.load_extension("cogs.jooble_bot_cogs")
         await self.load_extension("cogs.sandwich")
         await self.load_extension("cogs.python_commands")
